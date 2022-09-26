449acc07e8064be8f2b73860635a283747f68e4b
Дмитрий Коваленко <d.kovalenko.nt@gmail.com>
2022-09-26 17:04:45 +0300

    added methods to divide cells diagonally some cleanups in params calculation

--- graphic_environment/table_drawer.py
+++ graphic_environment/table_drawer.py
@@ -1,18 +1,14 @@
 from PIL import Image, ImageDraw, ImageFont
-from conf import PATH, user_path
-from conf import OUTLINE_WIDTH, INLINE_WIDTH, SMALLEST_STRING
-from conf import C_RED, C_GREEN, C_GREY
-from conf import RED_TAGS, GREEN_TAGS, GREY_TAGS
+from graphic_environment.conf import OUTLINE_WIDTH, INLINE_WIDTH, SMALLEST_STRING
+from graphic_environment.conf import C_RED, C_GREEN, C_GREY
+from graphic_environment.conf import RED_TAGS, GREEN_TAGS, GREY_TAGS
 
 
 class TableDrawer:
     @staticmethod
-    def draw_table(board, font_size, color_matrix=None, empty_matrix=None):
-        # PATH is defined in project, user_path is user's path to project directory
-        path = user_path + PATH
-
+    def draw_table(board, font_size, path_to_pic, color_matrix=None, double_cells=None):
         # calculates the width and height of image
-        params = TableDrawer.board_params(board, font_size)
+        params = TableDrawer.board_params(board, font_size, double_cells)
         x_size = len(params[0])
         y_size = len(params[1])
         width = OUTLINE_WIDTH * 2 + INLINE_WIDTH * (len(params[0]) - 1)
@@ -56,7 +52,7 @@ class TableDrawer:
                 for y in range(y_size):
                     seed = (start_pix + shift_x, start_pix + shift_y)
                     shift_y += params[1][y] + INLINE_WIDTH
-                    if (color_matrix[y][x] is tuple) and (len(color_matrix[y][x]) == 3):
+                    if (type(color_matrix[y][x]) is tuple) and (len(color_matrix[y][x]) == 3):
                         ImageDraw.floodfill(img, seed, color_matrix[y][x])
                         continue
                     if color_matrix[y][x] in GREY_TAGS:
@@ -69,10 +65,6 @@ class TableDrawer:
         # endregion
 
         # region printing text
-        if empty_matrix is None:
-            empty_matrix = [[1 for i in range(len(params[0]))] for j in range(len(params[1]))]
-        if empty_matrix == 'default':
-            empty_matrix = TableDrawer.create_default_empty_matrix(board)
         fnt = ImageFont.truetype('times.ttf', font_size)
         start_pix = OUTLINE_WIDTH
         shift_x = 0
@@ -82,31 +74,84 @@ class TableDrawer:
             for y in range(y_size):
                 fnt_seed = (start_pix + shift_x + params[0][x] / 2, start_pix + shift_y + params[1][y] / 2)
                 shift_y += params[1][y] + INLINE_WIDTH
-                if empty_matrix[y][x] == 1:
-                    draw.text(fnt_seed, str(board[y][x]), font=fnt, fill=0, anchor='mm')
+                draw.text(fnt_seed, str(board[y][x]), font=fnt, fill=0, anchor='mm')
             shift_x += params[0][x] + INLINE_WIDTH
         # endregion
 
-        img.show()
-        img.save(path + 'abaka_table.png', 'PNG')
+        TableDrawer.post_print(draw, font_size // 2, params, double_cells)
+
+        # img.show()
+        img.save(path_to_pic, 'PNG')
+        return path_to_pic
 
     @staticmethod
-    def board_params(board, font_size):
+    def post_print(draw, font_size, params, double_cells=None):
+        if double_cells is None:
+            return
+        fnt = ImageFont.truetype('times.ttf', font_size)
+        for d_cell in double_cells:
+            c_x, c_y = d_cell[0], d_cell[1]
+            word_bot, word_top = d_cell[2], d_cell[3]
+            l_x, t_y, r_x, b_y = TableDrawer.get_cell_corners(c_x, c_y, params)
+            draw.line((l_x, t_y, r_x, b_y), fill=0, width=INLINE_WIDTH)
+            bot_seed = ((r_x - l_x) / 4, (b_y - t_y) * 3 / 4)
+            top_seed = ((r_x - l_x) * 3 / 4, (b_y - t_y) / 4)
+            draw.text(bot_seed, str(word_bot), font=fnt, fill=0, anchor='mm')
+            draw.text(top_seed, str(word_top), font=fnt, fill=0, anchor='mm')
+
+    @staticmethod
+    def get_cell_corners(c_x, c_y, params):
+        start_pix = OUTLINE_WIDTH
+        shift_x = start_pix
+        shift_y = start_pix
+        for x in range(c_x):
+            shift_x += params[0][x] + INLINE_WIDTH
+        for y in range(c_y):
+            shift_y += params[1][y] + INLINE_WIDTH
+        return (shift_x, shift_y, shift_x + params[0][c_x], shift_y + params[1][c_y])
+
+    @staticmethod
+    def board_params(board, font_size, double_cells=None):
         # first array in params stands for x-size of cells, second for y-size
         # x and y are inverted because board is transposed to x-y basis
         params = [[], []]
         fnt = ImageFont.truetype('times.ttf', font_size)
         x_len = len(board)
         y_len = len(board[0])
+
+        # d_cell = [x-coord, y-coord, bottom word, top word]
+        double_matrix = [['' for i in range(y_len)] for j in range(x_len)]
+        if double_cells is not None:
+            half_fnt = ImageFont.truetype('times.ttf', font_size // 2)
+            for d_cell in double_cells:
+                c_x, c_y = d_cell[0], d_cell[1]
+                word_bot, word_top = d_cell[2], d_cell[3]
+                box_bot, box_top = half_fnt.getbbox(str(word_bot)), half_fnt.getbbox(str(word_top))
+                max_box_x, max_box_y = max(box_bot[2], box_top[2]), max(box_bot[3], box_top[3])
+                double_matrix[c_x][c_y] = (max_box_x, max_box_y)
+
         for y in range(y_len):
-            box = fnt.getbbox(str(board[0][y]))
-            params[0].append(box[2])
+            max_x = fnt.getbbox(SMALLEST_STRING)[2]
+            for x in range(x_len):
+                if double_matrix[x][y] != '':
+                    word_size = double_matrix[x][y][0]
+                else:
+                    word = str(board[x][y])
+                    word_size = fnt.getbbox(word.lower())[2]
+                if word_size > max_x:
+                    max_x = word_size
+            params[0].append(max_x)
         for x in range(x_len):
-            if fnt.getbbox(str(board[x][0]))[1] < fnt.getbbox(SMALLEST_STRING)[1]:
-                box = fnt.getbbox(SMALLEST_STRING)
-            else:
-                box = fnt.getbbox(str(board[x][0]))
-            params[1].append(box[3])
+            max_y = fnt.getbbox(SMALLEST_STRING)[3]
+            for y in range(y_len):
+                if double_matrix[y][x] != '':
+                    word_size = double_matrix[y][x][1]
+                else:
+                    word = str(board[y][x])
+                    word_size = fnt.getbbox(word.lower())[3]
+                if word_size > max_y:
+                    max_y = word_size
+            params[1].append(max_y)
 
         # resizes params arrays to make cells square if they look like high rectangle (x < y)
         min_y = 1000000
@@ -119,9 +164,9 @@ class TableDrawer:
 
         # makes cells larger to fit the text with gaps
         for y in range(y_len):
-            params[0][y] += 2 * int(font_size / 4)
+            params[0][y] += 2 * int(font_size // 4)
         for x in range(x_len):
-            params[1][x] += 2 * int(font_size / 4)
+            params[1][x] += 2 * int(font_size // 4)
 
         return params
 
@@ -143,6 +188,7 @@ class TableDrawer:
 
     # default empty matrix: 0 - empty, 1 - print text
     # in default '' or ' ' in board stands for empty
+    # old, probably useless method !OLD!
     @staticmethod
     def create_default_empty_matrix(board):
         empty_matrix = [[0 for i in range(len(board[0]))] for j in range(len(board))]
@@ -155,14 +201,14 @@ class TableDrawer:
         return empty_matrix
 
 
-board_mask = [["hello", '', '', '', ''],
-              [1, 1, 1, '', ''],
-              [1, 1, -1, -1, -1],
-              ['', '', -1, -1, 1],
-              ['', '', '', '', '']]
-board_color_mask = [[0, 0, 0, 0, 0],
-                    [1, 1, 1, 0, 0],
-                    [1, 1, -1, -1, -1],
-                    [0, 0, -1, -1, 1],
-                    [0, 0, 0, 0, 0]]
-TableDrawer.draw_table(board_mask, 40, color_matrix=board_color_mask, empty_matrix='default')
+# board_mask = [['', '', '', '', ''],
+#               [1, 1, 1, '', ''],
+#               [1, 1, -1, -1, -1],
+#               ['hello', '', -1, -1, 1],
+#               ['', '', '', '', '']]
+# board_color_mask = [[0, 0, 0, 0, 0],
+#                     [1, 1, 1, 0, 0],
+#                     [1, 1, -1, -1, -1],
+#                     [0, 0, -1, -1, 1],
+#                     [0, 0, 0, 0, 0]]
+# TableDrawer.draw_table(board_mask, 40, color_matrix=board_color_mask, empty_matrix='default')
